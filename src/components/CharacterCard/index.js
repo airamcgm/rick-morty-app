e128eb6cecfc134fe701376f0df4ca3f6ca8c09e
Carmen Garcia <airamcgmc@gmail.com>
2021-01-23 13:22:07 -0600

    add some styles

--- src/components/CharacterCard/index.js
+++ src/components/CharacterCard/index.js
@@ -1,20 +1,27 @@
 import React from 'react';
 
+import { FaHeartbeat } from 'react-icons/fa';
+import { GiHumanTarget } from 'react-icons/gi';
+import { RiAliensFill } from 'react-icons/ri';
+import { MdLocationCity } from 'react-icons/md';
+import { SiMicrogenetics } from 'react-icons/Si';
+import { TiLocation } from 'react-icons/ti';
+
 import "./styles.scss";
 
 const CharacterCard = ({ character }) => {
 
     return (
         <div className="card font_lato">
-            <img className="image" src={character.image} alt={character.name} width="30%"/>
-            <div className="column">
+            <div className="column text">
                 <p className="font_barriecito title">{character.name}</p>
-                <p>{character.status} - {character.species}</p>
-                {character.type!=""? (<p>Type: {character.type}</p>) : "" }
-                <p>Origin: {character.origin.name}</p>
-                <p>Location: {character.location.name}</p>
-                
+                <p>{character.status=="Alive"? <FaHeartbeat className="app_color_red"/> : <FaHeartbeat className="app_color_dark_grey"/>}<span>{character.status}</span></p>
+                <p>{character.species=="Human"? <GiHumanTarget className="app_color_blue"/> : <RiAliensFill className="app_color_green"/>}<span>{character.species}</span></p>
+                {character.type!="" ? <p><SiMicrogenetics className="app_color_purple"/><span>{character.type}</span></p> : "" }
+                <p><MdLocationCity className="app_color_dark_grey"/><span>{character.origin.name}</span></p>
+                <p><TiLocation className="app_color_dark_grey"/><span>{character.location.name}</span></p>
             </div>
+            <img className="image" src={character.image} alt={character.name} width="200px"/>
         </div>
     );
 }
