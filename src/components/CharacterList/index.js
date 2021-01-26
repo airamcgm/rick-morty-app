b9ad226b983209146d637bc81e6e2f8cd544ce35
Carmen Garcia <airamcgmc@gmail.com>
2021-01-26 17:03:20 -0600

    Add previous button

--- src/components/CharacterList/index.js
+++ src/components/CharacterList/index.js
@@ -3,47 +3,62 @@ import React, { useEffect } from 'react';
 import CharacterCard from '../CharacterCard';
 
 import { characterEntries } from '../../redux/CharacterList/selectors';
-import { nextUrl } from '../../redux/CharacterList/selectors';
+import { nextUrl, prevUrl } from '../../redux/CharacterList/selectors';
 import { fetchCharacterList } from '../../redux/CharacterList/actions';
 
 import { connect } from 'react-redux';
 
 import "./styles.scss";
 
-const CharacterList = ({ characterListState, fetchCharacterList, nextUrlState }) => {
+
+
+const CharacterList = ({ characterListState, fetchCharacterList, nextUrlState, prevUrlState }) => {
     const characterList = characterListState;
     const nextUrl = nextUrlState;
-    var nextUrlId = 0;
+    const prevUrl = prevUrlState;
 
     useEffect(() => {
         fetchCharacterList();
     }, [fetchCharacterList]);
 
-    useEffect(() => {
-        if(nextUrl!=null){
-            console.log('hola',nextUrl)
-            nextUrlId = nextUrl.charAt(nextUrl.length - 1)
-            console.log('hola',nextUrlId)
+    const nextPage = (page) => {
+        var nextUrlId = 0;
+        if(page!=null){
+            nextUrlId = page.substr(48,page.length);
         } 
-    });
+        console.log(nextUrlId)
+        fetchCharacterList(nextUrlId);
+    }
+    const previousPage = (page) => {
+        var prevUrlId = 0;
+        if(page!=null){
+            prevUrlId = page.substr(48,page.length);
+        } 
+        console.log(prevUrlId)
+        fetchCharacterList(prevUrlId);
+    }
 
     return (
         <div className="app_container app_grid" >
-
             {characterList.map((tile, id) => (
                 <CharacterCard character={tile} key={id}/>
             ))}
+            <button onClick={() => previousPage(prevUrl)}>Previous</button>
+            <button onClick={() => nextPage(nextUrl)}>Next</button>
         </div>
     );
 }
 
 const mapStateToProps = (state) => ({
     characterListState: characterEntries(state),
-    nextUrlState: nextUrl(state)
+    nextUrlState: nextUrl(state),
+    prevUrlState: prevUrl(state),
 });
   
   const mapDispatchToProps = (dispatch) => ({
-    fetchCharacterList: () => dispatch(fetchCharacterList(1))
+    fetchCharacterList: (page) => dispatch(fetchCharacterList(page)),
+    //fetchMoreCharacters: ({page}) => dispatch(fetchCharacterList(page))
+
 });
 
 export default connect(mapStateToProps,mapDispatchToProps)(CharacterList);
--- src/redux/CharacterList/selectors.js
+++ src/redux/CharacterList/selectors.js
@@ -5,3 +5,4 @@ const getCharacterList = (state) => get(state, "characterList");
 
 export const characterEntries = createSelector([getCharacterList], (list) => Object.values(list.results));
 export const nextUrl = createSelector([getCharacterList], (list) => list.info.next);
+export const prevUrl = createSelector([getCharacterList], (list) => list.info.prev);
